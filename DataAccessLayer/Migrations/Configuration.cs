845de1300e0b2547602e0fc486460ff7e2965495
ilhanbayazit <117454751+ilhanbayazit@users.noreply.github.com>
2026-02-20 13:10:27 +0300

    Add project files.

---
+++ DataAccessLayer/Migrations/Configuration.cs
@@ -0,0 +1,29 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DataAccessLayer.Concrate.Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+
+            TargetDatabase = new DbConnectionInfo(
+                @"Data Source=ilhan\SQLEXPRESS;Initial Catalog=DbMvcProje;Integrated Security=True",
+                "System.Data.SqlClient"
+            );
+        }
+
+        protected override void Seed(DataAccessLayer.Concrate.Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ EntityLayer/Concrate/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrate
+{
+    public class Contact
+    {
+        [Key]
+        public int ContantID { get; set; }
+        [StringLength(50)]
+        public string UserSurname { get; set; }
+        [StringLength(50)]
+
+        public string UserMail { get; set; }
+        [StringLength(50)]
+
+        public string UserSubject { get; set; }
+        [StringLength(500)]
+
+        public string UserMessage { get; set; }
+    }
+}
