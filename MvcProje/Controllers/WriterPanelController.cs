9ef8c299801dd5ba4be689a4a6261babba5c306e
ilhanbayazit <117454751+ilhanbayazit@users.noreply.github.com>
2026-05-09 16:11:56 +0300

    Writer Panelinde Profilim kısmı eklendi

--- MvcProje/Controllers/WriterPanelController.cs
+++ MvcProje/Controllers/WriterPanelController.cs
@@ -1,14 +1,16 @@
 ﻿using BusinessLayer.Concrate;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.Concrate;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrate;
+using FluentValidation.Results;
+using PagedList;
+using PagedList.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using PagedList;
-using PagedList.Mvc;
 
 namespace MvcProje.Controllers
 {
@@ -18,10 +20,41 @@ namespace MvcProje.Controllers
         // GET: WriterPanel
         HeadingManager hm = new HeadingManager(new EfHeadingDal());
         CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        WriterManager wm = new WriterManager(new EfWriterDal());
         Context c = new Context();
+        [HttpGet]
         public ActionResult WriterProfile()
         {
+            string p = (string)Session["WriterMail"];
+
+            int cd = c.Writers
+                .Where(x => x.WriterMail == p)
+                .Select(y => y.WriterID)
+                .FirstOrDefault();
+
+            var writer = wm.GetByID(cd);
+
+            return View(writer);
+        }
+        [HttpPost]
+        public ActionResult WriterProfile(Writer p)
+        {
+            WriterValidator writervalidator = new WriterValidator();
+            ValidationResult result = writervalidator.Validate(p);
+            if (result.IsValid)
+            {
+                wm.UpdateWriter(p);
+                return RedirectToAction("AllHeadings","WriterPanel");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
             return View();
+
         }
 
         public ActionResult MyHeading(string p)
