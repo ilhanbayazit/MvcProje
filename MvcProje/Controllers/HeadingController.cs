59f637aa336410870afafe9e7a1d8266fd6a80fb
ilhanbayazit <117454751+ilhanbayazit@users.noreply.github.com>
2026-05-11 23:23:42 +0300

    IFrame ile harita eklendi Raporlama sayfası eklendi

--- MvcProje/Controllers/HeadingController.cs
+++ MvcProje/Controllers/HeadingController.cs
@@ -21,6 +21,12 @@ namespace MvcProje.Controllers
             var headingValues = hm.GetList();
             return View(headingValues);
         }
+        public ActionResult HeadingReport()
+        {
+
+            var headingValues = hm.GetList();
+            return View(headingValues);
+        }
         [HttpGet]
         public ActionResult AddHeading()
         {
